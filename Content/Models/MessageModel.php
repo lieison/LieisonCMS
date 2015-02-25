f11dad1444efba390e34db98e9a90dc878bc948f
rolando arriaza <rolignu90@gmail.com>
2015-02-24 19:55:27 -0600

    front

--- Content/Models/MessageModel.php
+++ Content/Models/MessageModel.php
@@ -16,8 +16,10 @@ abstract class MessageModel extends MysqlConection {
     public abstract function SetMessage($id_u_para , $id_u_de  , $mensaje ,  $asunto = null);
     public abstract function SetSubmessage($id_message , $id_usuario , $mensaje);
     public abstract function GetMessageFrom($id_u_para , $limit = null);
-    public abstract  function GetMessageTo($id_u_de);
+    public abstract  function GetMessageTo($id_u_de , $limit = null) ;
     public abstract  function GetMessageCountFrom($id_u_para , $no_read = true);
     public abstract  function DeleteMessage($id_message);
     public abstract  function DeleteSubMessage($id_submesage);
+    public abstract  function GetSubMessage($id_mensaje);
+    public abstract  function GetCountSubMessage($id_mensaje  , $id_user, $not_read = true);
 }
