01b3ba47a777fd594e564713057bb924fb5678d8
rolignu2 <rolignu90@gmail.com>
2015-07-02 08:55:20 -0600

    merger

---
+++ Content/Web/admin/error/login/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include   '../../../../Conf/Include.php';
+  
+
+echo "Error en la conexion de la base de datos ... ";
+echo "<br>";
+echo "<a href='" . FunctionsController::GetUrl("login/") . "'>Volver</a>";
+
+
